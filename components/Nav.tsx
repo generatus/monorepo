364f2f3299097fc3b02db58dd33fe3a4a9109ef9
Alex Adewole <alex@bizar.re>
2021-09-27 13:36:21 -0700

    begin wallet/web3 integration

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,10 +1,37 @@
+import * as web3 from '@solana/web3.js';
+import {useState, useEffect} from 'react'
+
+const getProvider = () => {
+  if ("solana" in window) {
+    const provider = window.solana;
+    if (provider.isPhantom) {
+      return provider;
+    }
+  }
+  window.open("https://phantom.app/", "_blank");
+};
+
 const Nav = () => {
+  const [account, setAccount] = useState()
+
+  const login = () => {
+    const provider = getProvider();
+    if (provider) {
+      provider.on("connect", () => {
+        setAccount(provider.publicKey.toString())
+        // TODO: redirect to main page
+      })
+      
+      provider.connect()
+    }
+  }
+  
   return (
     <nav className="flex justify-between items-center p-5 max-w-screen-xl mx-auto">
       <div>generatus</div>
 
       <div>
-        <button>Sign Up</button>
+        { account ? account : <button onClick={login}>Log in</button> }
       </div>
     </nav>
   )
