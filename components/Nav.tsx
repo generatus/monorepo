efaa3d2f03e9e12e5fad09a6b99aa9be77882a76
Anthony Morris <amorriscode@gmail.com>
2021-09-27 20:21:37 -0700

    Fix types

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,38 +1,39 @@
-import * as web3 from '@solana/web3.js';
-import {useState, useEffect} from 'react'
+import * as web3 from '@solana/web3.js'
+import { useState } from 'react'
 
 const getProvider = () => {
-  if ("solana" in window) {
-    const provider = window.solana;
+  if (window.solana) {
+    const provider = window.solana
+
     if (provider.isPhantom) {
-      return provider;
+      return provider
     }
   }
-  window.open("https://phantom.app/", "_blank");
-};
+
+  // TODO: Messaging around using Phantom
+  window.open('https://phantom.app/', '_blank')
+}
 
 const Nav = () => {
   const [account, setAccount] = useState()
 
   const login = () => {
-    const provider = getProvider();
+    const provider = getProvider()
+
     if (provider) {
-      provider.on("connect", () => {
+      provider.on('connect', () => {
         setAccount(provider.publicKey.toString())
-        // TODO: redirect to main page
       })
-      
+
       provider.connect()
     }
   }
-  
+
   return (
     <nav className="flex justify-between items-center p-5 max-w-screen-xl mx-auto">
       <div>generatus</div>
 
-      <div>
-        { account ? account : <button onClick={login}>Log in</button> }
-      </div>
+      <div>{account ? account : <button onClick={login}>Log in</button>}</div>
     </nav>
   )
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,6 @@ import '../styles/globals.css'
 
 import type { AppProps } from 'next/app'
 
-import Layout from 'components/Layout'
-
 function GeneratusWeb({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
